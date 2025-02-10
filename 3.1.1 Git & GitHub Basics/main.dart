e921f062eaef1c6245b36b364685fa1f7fbf3bd6
microwalker <microwalker@gmx.net>
2025-02-10 16:02:11 +0100

    extraction method

--- 3.1.1 Git & GitHub Basics/main.dart
+++ 3.1.1 Git & GitHub Basics/main.dart
@@ -1,11 +1,17 @@
 import 'tutor.dart';
 import 'tuts.dart';
 
+tuts Tutoren = new tuts();
+
 void main(List<String> args) {
-  tuts Tutoren = new tuts();
-  tutor Lehrer = new tutor();
-  Lehrer.name = "David";
-  Lehrer.addFach("Coding Basics");
-  Tutoren.addTutor(Lehrer);
+  addTutor("Jean Paul", ["Figma", "UI/UX-Design"]);
+  addTutor("David", ["Coding", "Git", "GitHub"]);
   print(Tutoren.toString());
+}
+
+void addTutor(String name, List faecher) {
+  tutor Lehrer = new tutor();
+  Lehrer.name = name;
+  faecher.forEach((fach) => Lehrer.addFach(fach));
+  Tutoren.addTutor(Lehrer);
 }
