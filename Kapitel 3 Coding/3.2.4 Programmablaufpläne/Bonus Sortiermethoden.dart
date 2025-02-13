d77ce4bf6ec4aa91c86b79432408652b4501d2a5
microwalker <microwalker@gmx.net>
2025-02-13 19:53:17 +0100

    added wordlist

--- Kapitel 3 Coding/3.2.4 Programmablaufpläne/Bonus Sortiermethoden.dart
+++ Kapitel 3 Coding/3.2.4 Programmablaufpläne/Bonus Sortiermethoden.dart
@@ -1,21 +1,34 @@
 import 'dart:math';
 
 void main(List<String> args) {
-  List liste = []; [9,84,36,71,12,8,44,69,32,108,11,71,14,3,92,22,79,37,3,19,102];
-  for(int i=0;i<1000;i++)
-    liste.add((Random().nextInt(10000)));
+  List liste = []; // [9,84,36,71,12,8,44,69,32,108,11,71,14,3,92,22,79,37,3,19,102];
+  GenerateRandomsIntNumbers(liste, 100, 1000);
+  startCompetitiom(liste);
+
+  /* benötigt Modifikation der Sort-Routinen zum dynamischen Vergleich von Strings, Stichwort (a,b) => b.compareTo(a) 
+  fillWithWords(liste);
+  startCompetitiom(liste);
+  */
+}
+
+void startCompetitiom(List<dynamic> liste) {
   Stopwatch c = new Stopwatch();
   print("Original: $liste");
-  c.start();
+  c.start(); // Zeitmessung für BubbleSort starten
   print("Bubble: ${BubbleSort(liste.toList())} (Dauer: ${c.elapsed})");
-  c.reset();
+  c.reset(); // Zeitmessung für SelectionSort neu starten
   print("Selection ${SelectionSort(liste.toList())} (Dauer: ${c.elapsed})");
-  c.reset();
+  c.reset(); // Zeitmessung für ExtSelectionSort neu starten
   print("ExtSelection ${ExtSelectionSort(liste.toList())} (Dauer: ${c.elapsed})");
-  c.reset();
+  c.reset(); // Zeitmessung für List.Sort() neu starten
   liste.sort();
   print("List.Sort: $liste (Dauer: ${c.elapsed})");
-  c..stop();
+  c..stop(); // Stopuhr beenden
+}
+
+void GenerateRandomsIntNumbers(List list, int anzahl, int max) {
+  for(int i=0;i<anzahl;i++)
+    list.add((Random().nextInt(max)));
 }
 
 List BubbleSort(List liste) {
@@ -81,4 +94,23 @@ List ExtSelectionSort(List liste) {
   }
   print("ExtSelectionSort: $swaps Swaps / $loops Loops");
   return liste;
+}
+
+void fillWithWords(List list) {
+  list = ["Erwachsener","Flugzeug","Luft","Flugzeugträger","Luftwaffe","Flughafen","Album","Alphabet","Apfel","Arm","Meer","Baby","Baby","Rucksack",
+          "Ballon","Banane","Bank","Grill","Bad","Badewanne","Bett","Biene","Bibel","Vogel","Bombe","Buch","Boss","Flasche","Schüssel","Box","Junge",
+          "Gehirn","Brücke","Schmetterling","Taste","Cappuccino","Wagen","Autorennen","Teppich","Karotte","Höhle","Stuhl","Schachbrett","Chef","Kind",
+          "Meißel","Pralinen","Kirche","Kreis","Zirkus","Zirkus","Uhr","Clown","Kaffee","Café","Unkraut","Komet","Compact Disc","Kompass","Computer",
+          "Kristall","Tasse","Zyklus","Datenbank","Schreibtisch","Diamant","Kleid","Bohren","Trinken","Trommel","Dung","Ohren","Erde","Ei","Elektrizität",
+          "Elefant","Radiergummi","Explosiv","Augen","Familie","Ventilator","Feder","Festival","Film","Finger","Feuer","Scheinwerfer","Blume","Fuß","Gabel",
+          "Obst","Pilz","Spiel","Garten","Gas","Tor","Edelstein","Mädchen","Handschuhe","Gott","Trauben","Gitarre","Hammer","Hut","Hieroglyphe","Autobahn",
+          "Horoskop","Pferd","Schlauch","Eis","Insekt","Düsenjäger","Junk","Kaleidoskop","Küche","Messer","Lederjacke","Bein","Bibliothek","Flüssigkeit",
+          "Magnet","Mann","Karte","Matze","Fleisch","Meteor","Mikroskop","Milch","Milchshake","Nebel","Geld","Monster","Moskito","Mund","Nagel","Marine",
+          "Halskette","Nadel","Zwiebel","Pinsel","Hose","Fallschirm","Reisepass","Kieselstein","Pendel","Pfeffer","Parfüm","Kissen","Flugzeug","Planet",
+          "Tasche","Postamt","Kartoffel","Drucker","Gefängnis","Pyramide","Radar","Regenbogen","Aufzeichnung","Restaurant","Gewehr","Ring","Roboter",
+          "Felsen","Rakete","Dach","Zimmer","Seil","Sattel","Salz","Sandpapier","Sandwich","Satellit","Schule","Sex","Schiff","Schuhe","Geschäft",
+          "Dusche","Unterschrift","Skelett","Sklave","Schnecke","Software","Fester Körper","Space Shuttle","Spektrum","Kugel","Würzen","Spiral","Löffel",
+          "Sportwagen","Scheinwerfer","Quadrat","Treppe","Star","Bauch","Sonne","Sonnenbrille","Landvermesser","Schwimmbad","Schwert","Tabelle","Tapisserie",
+          "Zähne","Fernrohr","Fernsehen","Tennisschläger","Thermometer","Tiger","Toilette","Zunge","Fackel","Torpedo","Zug","Laufband","Dreieck","Tunnel",
+          "Schreibmaschine","Regenschirm","Vakuum","Vampir","Videoband","Geier","Wasser","Waffe"];
 }
