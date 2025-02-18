e3890e4717651d6c31de50af66be958b9e0a45e5
microwalker <microwalker@gmx.net>
2025-02-18 11:04:16 +0100

    added 3.3.3 Variablen und Konstanten

---
+++ Kapitel 3 Coding/3.3.3 Variablen und Anweisungen/main.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  String c = "Schöne";
+  String d = "!";
+  String dayStr = "Tag";
+  String gdStr = "Guten";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+  String timeStr = "Zeit";
+  String worldStr = "Welt";
+  
+  int a = 24;
+  int d = 20; // FAIL: d wird schon als String-Variable verwendet! Absicht?
+  int seven = 7;
+  int twelve = 12;
+}
