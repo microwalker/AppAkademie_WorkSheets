8dd48e602f0402fc6bf446b0ffc429e018d8a2e9
microwalker <microwalker@gmx.net>
2025-03-10 11:35:03 +0100

    added bonusAufg2

--- Kapitel 3 Coding/3.5.1 Schleifen II/bonus.dart
+++ Kapitel 3 Coding/3.5.1 Schleifen II/bonus.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 void main() {
   bonusAufg1();
+  bonusAufg2();
 }
 
 void bonusAufg1() {
@@ -9,10 +12,28 @@ void bonusAufg1() {
   }
 }
 
+void bonusAufg2() {
+  List<int> zahlen = [5, 14, 7, 2, 9];
+  for(int zahl in zahlen) {
+    printForwardAndBackAgain(zahl);
+  }
+}
+
 bool isPalindrom(String word) {
   String reversedWord = "";
   for(int i=word.length;i>0;i--) {
     reversedWord += word[i-1];
   }
   return reversedWord.toLowerCase() == word.toLowerCase();
+}
+
+void printForwardAndBackAgain(int zahl) {
+  String fb = "";
+  for(int i=1;i<=zahl;i++) {
+    fb += "$i ";
+  }
+  for(int i=zahl-1;i>0;i--) {
+    fb += "$i ";
+  }
+  print(fb);
 }
