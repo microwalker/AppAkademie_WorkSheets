77fe209b4bc8b4e683dea21dd9e96582548d5e27
microwalker <microwalker@gmx.net>
2025-03-12 14:52:37 +0100

    added 3.5.2 Funktionen I

---
+++ Kapitel 3 Coding/3.5.2 Funktionen I/bonus.dart
@@ -0,0 +1,12 @@
+import 'funlib.dart';
+
+void main(List<String> args) {
+  // Bonusaufgabe 1
+  [185,421,67].forEach((mins) => convertMinutesToTime(mins));
+
+  // Bonusaufgabe 2
+  analyzeGrades([2,1,3]);
+
+  // Bonusaufgabe 3
+  analyzeNumbers([8, 21, -4, 99, 48, -15, 8, 76, 21, 0]);
+}
---
+++ Kapitel 3 Coding/3.5.2 Funktionen I/main.dart
@@ -0,0 +1,15 @@
+import "funlib.dart";
+
+void main(List<String> args) {
+  // Aufgabe 1:
+  [7,11,4,19].forEach((number) => printNumber(number)); 
+
+  // Aufgabe 2:
+  ["Gargel","Schraub","Würfel"].forEach((text) => triplePrint(text)); 
+
+  // Aufgabe 3:
+  List<List<String>> names = [["Johnny","Depp"],["Tom","Cruise"],["Klaus","Kinski"]];
+  for(List name in names)
+    reverseInitials(name[0], name[1]);
+}
+
