be6200608521a1017291e3e0d7cd0e53e6ad8218
microwalker <microwalker@gmx.net>
2025-04-01 21:08:09 +0200

    dynamic listtiles... and some "prettying"

--- Kapitel 4 Flutter/4.2.3 P13 Flutter App/p13_flutter_app/lib/main.dart
+++ Kapitel 4 Flutter/4.2.3 P13 Flutter App/p13_flutter_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,37 +12,47 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  final List<Map<String, dynamic>> _listElements = const [
+    {"key": "u1", "icon": Icons.face, "title": "User 1", "subtitle": "Der allererste"},
+    {"key": "u2", "icon": Icons.face_2, "title": "User 2", "subtitle": "Immerhin die Zweite"},
+    {"key": "u3", "icon": Icons.face_3, "title": "User 3", "subtitle": "Naja, wenigstens die Dritte"},
+    {"key": "u4", "icon": Icons.face_4, "title": "User 4", "subtitle": "Räbäh... nur Vierter"}];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: "MainApp", debugShowCheckedModeBanner: kDebugMode,
       home: Scaffold(
-        appBar: AppBar(title: Text("Flutter, Flatter..."), leading: IconButton.outlined(onPressed: null, icon: Icon(Icons.bathroom_outlined, color: Color.fromARGB(255, 213, 207, 22),)), centerTitle: true, backgroundColor: Color.fromARGB(197, 13, 86, 85),),
-        floatingActionButton: FloatingActionButton.extended(onPressed: () => Dialog(child: Text("Dialog")), icon: Icon(Icons.help_center_rounded), label: Text("What?")),
-        bottomNavigationBar: BottomNavigationBar(backgroundColor: Colors.deepOrangeAccent[200], currentIndex: 1, type: BottomNavigationBarType.fixed, items: [
+        appBar: AppBar(title: Text("Flutter, Flatter..."), leading: IconButton.outlined(onPressed: () {}, icon: Icon(Icons.bathroom_outlined, color: Color.fromARGB(255, 213, 207, 22),)), centerTitle: true, backgroundColor: Color.fromARGB(197, 13, 86, 85),),
+        floatingActionButton: FloatingActionButton.extended(onPressed: () {}, icon: Icon(Icons.help_center_rounded), label: Text("What?")),
+        bottomNavigationBar: BottomNavigationBar(onTap: (value) { print("Tabbed on $value");}, backgroundColor: Colors.deepOrangeAccent[200], currentIndex: 1, type: BottomNavigationBarType.fixed, items: [
           BottomNavigationBarItem(label: "Item 1", icon: Icon(Icons.access_alarm, color: Colors.tealAccent)), 
           BottomNavigationBarItem(label: "Item 2", icon: Icon(Icons.access_time, color: Colors.tealAccent)), 
           BottomNavigationBarItem(label: "Item 3", icon: Icon(Icons.ac_unit, color: Colors.tealAccent))]),
         drawer: Drawer(backgroundColor: Colors.blueAccent[600], elevation: 80, clipBehavior: Clip.hardEdge , child: Text("Inside Drawer")),
         drawerDragStartBehavior: DragStartBehavior.start,
-        persistentFooterButtons: [IconButton.outlined(onPressed: null, icon: Icon(Icons.adb))],
+        persistentFooterButtons: [IconButton.outlined(onPressed: () {}, icon: Icon(Icons.adb))],
         body: Center(
           child: Column(children: [
               Text("Golden times for Developers!", 
                 style: TextStyle(fontFamily: "CupertinoSystemDisplay", fontSize: 24, fontStyle: FontStyle.italic, 
                   fontWeight: FontWeight.w500, color: Color.fromARGB(255, 66, 60, 177))),
-              Checkbox(value: false, onChanged: null),
-              Switch(value: true, onChanged: null),
-              TextButton(onPressed: null, child: Text("Ich bin ein TextButton")),
-              FilledButton(onPressed: null, child: Text("Ich ein FilledButton")),
-              FilledButton.icon(onPressed: null, label: Text(" Mit Icon! Und Filled!"), icon: Icon(Icons.face_sharp, size: 36, color: Color.fromARGB(255, 173, 13, 13))),
-              Spacer(flex: 1),
-              ListBody(children: [ListTile(leading: Icon(Icons.face_5), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text("Listenelement 1")), subtitle: Text("Weil ich der erste in der Liste bin!")), ListTile(leading: Icon(Icons.face_2), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text("Listenelement 2")), subtitle: Text("Weil ich die zweite in der Liste bin!"))],),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Checkbox(value: false, onChanged:(value) {value ??= !value!;},side: BorderSide(width: 1.2), shape: StarBorder(innerRadiusRatio: 0.4, pointRounding: 0.5, rotation: 11, points: 6)), 
+                Switch(value: true, onChanged: (value) {}, thumbIcon: WidgetStatePropertyAll(Icon(Icons.face_sharp))), 
+                FilledButton(onPressed: () {}, child: Text("Ich ein FilledButton"))]),
+              TextButton(onPressed: () {}, child: Text("Ich bin ein TextButton")),              
+              MyTextInput(),
+              // FilledButton.icon(onPressed: null, label: Text(" Mit Icon! Und Filled!"), icon: Icon(Icons.face_sharp, size: 36, color: Color.fromARGB(255, 173, 13, 13))),
+              Padding(padding: EdgeInsets.only(left: 16, right: 16),
+                child: ListBody(children: [ for(Map m in _listElements) 
+                  ListTile(key: Key(m["key"]), dense: false, onTap: () => onElementTab(m["key"]), hoverColor: Colors.teal, leading: Icon(m["icon"]), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text(m["title"])), subtitle: Text(m["subtitle"])) ]),
+                  // ListTile(onTap: () {}, hoverColor: Colors.teal, leading: Icon(Icons.face_3), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text("Listenelement 1")), subtitle: Text("Weil ich der erste in der Liste bin!")), 
+                  // ListTile(onTap: () {}, hoverColor: Colors.teal, leading: Icon(Icons.face_2), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text("Listenelement 2")), subtitle: Text("Weil ich die zweite in der Liste bin!"))],),
+              ),
               // ListView(children: [ListTile(leading: Icon(Icons.face_5), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text("Listenelement 1")), subtitle: Text("Weil ich der erste in der Liste bin!")), ListTile(leading: Icon(Icons.face_2), title: Title(color: Color.fromARGB(255, 20, 65, 5), child: Text("Listenelement 2")), subtitle: Text("Weil ich die zweite in der Liste bin!"))],),
               Divider(thickness: 3),
               MyTransform(),
-              Spacer(flex: 1),
-              IconButton.outlined(onPressed: null, icon: Icon(Icons.shopping_bag_outlined, color: Color.fromARGB(160, 160, 13, 13))),
-              Spacer(flex: 2)]
+              IconButton.outlined(onPressed: () {}, icon: Icon(Icons.shopping_bag_outlined, color: Color.fromARGB(160, 160, 13, 13))) ]
           ),
         ), 
         endDrawer: EndDrawerButton(),
@@ -49,6 +60,8 @@ class MainApp extends StatelessWidget {
       )
     );
   }
+
+  void onElementTab(String key) => print(key);
 }
 
 class MyTransform extends StatefulWidget { 
@@ -75,5 +88,29 @@ class _MyTransformState extends State<MyTransform> {
   void changeRotation(double value) => setState(() {
     _angle = value.toInt();
   });
+}
+
+class MyTextInput extends StatefulWidget {
+  const MyTextInput({super.key});
+
+  
+
+  @override
+  State<MyTextInput> createState() => _MyTextInputState();
+}
+
+class _MyTextInputState extends State<MyTextInput> {
+  String _text = "";
+
+  String get text => _text;
+  
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: 320,
+      child: TextField(onSubmitted: (value) => _text = value, style: TextStyle(color: Colors.limeAccent, fontWeight: FontWeight.bold),
+        decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.elliptical(16, 16))), 
+        labelText: "Gib was ein!", filled: true, fillColor: Colors.teal, hintText: "...muss auch keinen Sinn ergeben!")),
+    );
+  }
 }
 
